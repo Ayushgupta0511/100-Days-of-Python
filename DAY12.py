12152384c9a04f2da2a092cf82389858b36aadff
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-02 23:46:58 +0530

    Add files via upload

---
+++ DAY12.py
@@ -0,0 +1,4 @@
+nm = "harry*******"
+#print(nm[-3:-2])
+print(nm.rstrip("*"))
+print(nm.replace("harry**","AYUSH!!1"))
---
+++ DAy11.py
@@ -0,0 +1,6 @@
+x = ('''hey ayush 
+     how are you?
+     i am fine how are you?''')
+print(x)
+for characters in x:
+    print(characters)
