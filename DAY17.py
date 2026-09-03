9cf2c93a26b1db53f3c65c031a0f296283b073f3
AYUSH GUPTA <195641436+Ayushgupta0511@users.noreply.github.com>
2026-09-03 23:55:48 +0530

    Add files via upload

---
+++ DAY17.py
@@ -0,0 +1,8 @@
+# colours = [ "red", "green", "blue", "pink", "yellow"]
+# for x in colours:
+#     print(x)
+#     for i in x:
+#         print(i)
+
+for k in range(6):
+    print(k+4)
---
+++ DAY22.py
@@ -0,0 +1,8 @@
+marks = [ 2,3,2, "AYuSh",True]
+# print(marks[3])
+# print(marks[4])
+# print(marks(1))
+if "AYS" in "AYuSh":
+    print("yes")
+else:
+    print("nO")
